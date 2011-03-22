ff3e95ad60d506dc4a32d361cba949db36733bc5
Jason Urton <jason.urton@gmail.com>
2011-03-22 14:06:27 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GroceryGenie::Application.config.secret_token = '8dac5d581cfd71af3fab9c9c8de4cdc60fd5124355ef0b9579fc9f08cec0250ad56012b9688908cd7dde030527dd91d21463783335270d21f9539037c6f27f83'
